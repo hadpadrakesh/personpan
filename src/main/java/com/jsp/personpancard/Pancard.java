4e4427232ec32f101009d02ec85ff6dd0e6a5851
hadpadrakesh <rakeshhadpad026@gmail.com>
2023-06-03 13:04:46 +0530

    updated

--- src/main/java/com/jsp/personpancard/Pancard.java
+++ src/main/java/com/jsp/personpancard/Pancard.java
@@ -4,19 +4,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
-
 
 @Entity
-public class Person {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class Pancard {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String address;
-	private long phno;
-	@OneToOne
-    private Pancard pan;
+	
 	public int getId() {
 		return id;
 	}
@@ -35,22 +32,10 @@ public class Person {
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	public long getPhno() {
-		return phno;
-	}
-	public void setPhno(long phno) {
-		this.phno = phno;
-	}
-	public Pancard getPan() {
-		return pan;
-	}
-	public void setPan(Pancard pan) {
-		this.pan = pan;
-	}
 	@Override
 	public String toString() {
-		return "Person [id=" + id + ", name=" + name + ", address=" + address + ", phno=" + phno + ", pan=" + pan + "]";
+		return "Pancard [id=" + id + ", name=" + name + ", address=" + address + "]";
 	}
 	
-	
+
 }
